1e83b093f2d65614274954de5219e5c7bed2df89
Justin Stewart <stewartjustin3@gmail.com>
2020-06-30 18:42:00 -0400

    getting results from querymoved query to app.js

---
+++ src/Search.js
@@ -0,0 +1,33 @@
+import  React, { Component } from 'react';
+import * as BooksAPI from './BooksAPI'
+
+class Search extends Component{
+
+    render(){
+        const {handleSearchSubmit} = this.props;
+        return(
+            <div className="search-books">
+            <div className="search-books-bar">
+              <button className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</button>
+              <div className="search-books-input-wrapper">
+                {/*
+                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
+                  You can find these search terms here:
+                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+
+                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
+                  you don't find a specific author or title. Every search is limited by search terms.
+                */}
+                <input type="text" onChange={(e)=>handleSearchSubmit(e)} placeholder="Search by title or author"/>
+                    {console.log(this.state)}
+              </div>
+            </div>
+            <div className="search-books-results">
+              <ol className="books-grid"></ol>
+            </div>
+          </div>
+        )
+    }
+}
+
+export default Search;
